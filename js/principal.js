661f5fa3be64319a0f367d4c5a73f0fb3e126bb6
Alfonso Aguilar <aruponse@gmail.com>
2016-09-22 09:07:31 -0500

    Inicio

---
+++ js/principal.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	/*$('#footerCarousel').carousel({
+	interval: 10000
+	})
+    
+    $('#footerCarousel').on('slid.bs.carousel', function() {
+    	//alert("slid");
+	});
+*/
+	$('[data-toggle="popover"]').popover({ template : '<div class="popover" style="min-width: 480px;" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>' }); 
+});
